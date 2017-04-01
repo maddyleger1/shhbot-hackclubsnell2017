7c35d8ae556018a64c9c12e3097747efffb06093
Maddy Leger <maddyleger1@gmail.com>
2017-04-01 19:48:29 -0400

    arduino & particle code

---
+++ particle_txrx/particle_txrx.ino
@@ -0,0 +1,82 @@
+int led1 = D0;
+int led2 = D7;
+
+void setup()
+{
+
+   // Here's the pin configuration, same as last time
+   pinMode(led1, OUTPUT);
+   pinMode(led2, OUTPUT);
+
+   // We are also going to declare a Particle.function so that we can turn the LED on and off from the cloud.
+   Particle.function("led",ledToggle);
+   Particle.function("zoneSelect",moveToZone);
+   // This is saying that when we ask the cloud for the function "led", it will employ the function ledToggle() from this app.
+
+   // For good measure, let's also make sure both LEDs are off when we start:
+   digitalWrite(led1, LOW);
+   digitalWrite(led2, LOW);
+   
+   Serial1.begin(9600, SERIAL_9N1); // via TX/RX pins, 9600 9N1 mode
+
+}
+
+
+void loop()
+{
+   // Nothing to do here
+}
+
+
+int ledToggle(String command) {
+
+    if (command=="on") {
+        digitalWrite(led1,HIGH);
+        digitalWrite(led2,HIGH);
+        return 1;
+    }
+    else if (command=="off") {
+        digitalWrite(led1,LOW);
+        digitalWrite(led2,LOW);
+        return 0;
+    }
+    else {
+        return -1;
+    }
+}
+
+int moveToZone(String zone) {
+    
+    if (zone=="1") {
+        for (int i = 0; i < 5; i++) {
+            digitalWrite(led1,HIGH);
+            delay(1000);
+            digitalWrite(led1,LOW);
+            delay(1000);
+            Serial1.write(1);
+        }
+    } else if (zone=="2") {
+        for (int i = 0; i < 2; i++) {
+            digitalWrite(led1,HIGH);
+            delay(2000);
+            digitalWrite(led1,LOW);
+            delay(2000);
+            Serial1.write(2);
+        }
+    } else if (zone=="3") {
+        for (int i = 0; i < 10; i++) {
+            digitalWrite(led1,HIGH);
+            delay(500);
+            digitalWrite(led1,LOW);
+            delay(500);
+            Serial1.write(3);
+        }
+    } else {
+        digitalWrite(led2,HIGH);
+        delay(5000);
+        digitalWrite(led2,LOW);
+    }
+    
+    return 0;
+    
+}
