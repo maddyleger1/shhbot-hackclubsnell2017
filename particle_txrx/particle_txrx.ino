71def73805bc6c5151b3212ddf2b753e6f5653aa
Maddy Leger <maddyleger1@gmail.com>
2017-04-02 15:09:51 -0400

    comments in slackbot and particle

--- particle_txrx/particle_txrx.ino
+++ particle_txrx/particle_txrx.ino
@@ -1,22 +1,19 @@
-int led1 = D0;
-int led2 = D7;
+int led1 = D7; // on board led - if you don't want to use this LED, you can use port D0 for the built in particle one
 
 void setup()
 {
-
-   // Here's the pin configuration, same as last time
+   // LED configuration
    pinMode(led1, OUTPUT);
-   pinMode(led2, OUTPUT);
 
-   // We are also going to declare a Particle.function so that we can turn the LED on and off from the cloud.
+   // Declare a Particle.function to control Particle from the cloud
+   // Basically, it exposes functions inside this file to a cloud interface by the name in quotations
    Particle.function("led",ledToggle);
    Particle.function("zoneSelect",moveToZone);
-   // This is saying that when we ask the cloud for the function "led", it will employ the function ledToggle() from this app.
 
-   // For good measure, let's also make sure both LEDs are off when we start:
+   // make sure the led is off
    digitalWrite(led1, LOW);
-   digitalWrite(led2, LOW);
    
+   // open up the tx/rx Serial connection to the arduino
    Serial1.begin(9600, SERIAL_9N1); // via TX/RX pins, 9600 9N1 mode
 
 }
@@ -27,26 +24,8 @@ void loop()
    // Nothing to do here
 }
 
-
-int ledToggle(String command) {
-
-    if (command=="on") {
-        digitalWrite(led1,HIGH);
-        digitalWrite(led2,HIGH);
-        return 1;
-    }
-    else if (command=="off") {
-        digitalWrite(led1,LOW);
-        digitalWrite(led2,LOW);
-        return 0;
-    }
-    else {
-        return -1;
-    }
-}
-
 int moveToZone(String zone) {
-    
+    // zone is passed in from slackbot.js
     if (zone=="1") {
         Serial1.write(1);
     } else if (zone=="2") {
@@ -54,11 +33,11 @@ int moveToZone(String zone) {
     } else if (zone=="3") {
         Serial1.write(3);
     } else {
-        digitalWrite(led2,HIGH);
+        // this won't happen from the bot, but is here to acknowledge you if you give a wrong argument from the cloud
+        digitalWrite(led1,HIGH);
         delay(5000);
-        digitalWrite(led2,LOW);
+        digitalWrite(led1,LOW);
     }
-    
+    // will error check eventually
     return 0;
-    
 }
